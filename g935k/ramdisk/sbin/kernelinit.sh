5776372f3ea2c0dfc1172dbfbe681c057ddef94c
kykdev <kykint@naver.com>
2017-09-04 14:35:49 +0900

    Update security flag reset list

--- g935k/ramdisk/sbin/kernelinit.sh
+++ g935k/ramdisk/sbin/kernelinit.sh
@@ -20,6 +20,7 @@
 /sbin/resetprop -n ro.boot.verifiedbootstate "green";
 /sbin/resetprop -n ro.boot.flash.locked "1";
 /sbin/resetprop -n ro.boot.ddrinfo "00000001";
+/sbin/resetprop -n ro.build.selinux "1";
 
 /sbin/resetprop -n ro.fmp_config "1";
 /sbin/resetprop -n ro.boot.fmp_config "1";
